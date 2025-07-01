49334ac5b3fb68f2e18eaa3a0c615069364ffa3d
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2025-07-01 22:56:03 +0100

    Add database connection setup with Mongoose

--- user-service/app.js
+++ user-service/app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser');
 const userRoutes = require('./routes/user.routes');
+const connectDB = require('./db/db'); // Assuming you have a db config file
 
 const app = express();
 dotenv.config();
 app.use(cookieParser());
+
+connectDB();
 
 app.use('/', userRoutes);
 
---
+++ user-service/db/db.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+function connectDB() {
+    // mongoose.connect(process.env.MONGO_URL, {
+    //     useNewUrlParser: true,
+    //     useUnifiedTopology: true,
+    // })
+    mongoose.connect(process.env.MONGO_URL)
+        .then(() => {
+            console.log('MongoDB connected successfully');
+        })
+        .catch((err) => {
+            console.error('MongoDB connection error:', err);
+        });
+}
+
+module.exports = connectDB;
