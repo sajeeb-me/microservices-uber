ca7a2c612832990a43a9b36645e69039e62352d1
Sajeeb Das Shuvo <hellosdshuvo@gmail.com>
2025-07-16 18:56:46 +0100

    Add login and logout routes to user service

--- user-service/routes/user.routes.js
+++ user-service/routes/user.routes.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const userController = require('../controllers/user.controller');
 
 router.post('/register', userController.register);
+router.post('/login', userController.login);
+router.get('/logout', userController.logout);
 
 module.exports = router;
